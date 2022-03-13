6bd8026154e9b5915f7d2faec62a0474a664fe01
Ian <37228869+IanGlass@users.noreply.github.com>
2022-03-14 09:04:17 +1300

    Make e2e testing compatible with middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
@@ -11,6 +12,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/users.entity';
 import { Report } from './reports/reports.entity';
 import { AuthService } from './users/auth.service';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
@@ -24,6 +27,27 @@ import { AuthService } from './users/auth.service';
     }),
   ],
   controllers: [AppController, UsersController, ReportsController],
-  providers: [AppService, UsersService, ReportsService, AuthService],
+  providers: [
+    AppService,
+    UsersService,
+    ReportsService,
+    AuthService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: ['1ee708de-ae0f-4959-91dc-0563957c8811'],
+        }),
+      )
+      .forRoutes('*');
+  }
+}
