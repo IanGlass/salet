487d80e5643bbd8fa99c443f435c90ce051567d7
Ian <igla626@aucklanduni.ac.nz>
2022-04-11 09:47:20 +1200

    Add ability to create and approve a report

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,16 +1,28 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Param, Patch } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { CreateReportDTO } from './dtos/create-report.dto';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/guards/auth.guards';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/users.entity';
+import { ReportDto } from './dtos/report.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
 
-  @Post('/reports')
+  @Post('/create')
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDTO) {
-    return this.reportsService.create(body);
+  @Serialize(ReportDto)
+  createReport(@Body() body: CreateReportDTO, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
+  }
+
+  @Patch('/approve/:id')
+  @UseGuards(AuthGuard)
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportsService.approveReport(id, body.approved);
   }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './reports.entity';
 import { CreateReportDTO } from './dtos/create-report.dto';
+import { User } from 'src/users/users.entity';
 
 @Injectable()
 export class ReportsService {
@@ -11,8 +12,21 @@ export class ReportsService {
     private repo: Repository<Report>,
   ) {}
 
-  create(reportDto: CreateReportDTO) {
+  create(reportDto: CreateReportDTO, user: User) {
     const report = this.repo.create(reportDto);
+    report.user = user;
+
+    return this.repo.save(report);
+  }
+
+  async approveReport(id: string, approved: boolean) {
+    const report = await this.repo.findOne(id);
+
+    if (!report) {
+      throw new NotFoundException('Report not found');
+    }
+
+    report.approved = approved;
 
     return this.repo.save(report);
   }
